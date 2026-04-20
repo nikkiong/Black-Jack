f7e3a70652ada859d4b4acceea3dba4a73da6f26
Nicole (Nikki) Ong <134902567+nikkiong@users.noreply.github.com>
2026-04-20 02:19:02 -0700

    initial GUIManager class based on current Class Diagram

---
+++ GUIManager.java
@@ -0,0 +1,58 @@
+// components for GUI
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.io.File;
+import java.nio.file.Files;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+
+public class GUIManager {
+
+	private Client client;
+	private String currentScreen;
+	
+	public GUIManager() {
+		
+	}
+	
+	public void showLoginScreen() {
+		
+	}
+	
+	public void showLobbyScreen() {
+		
+	}
+	
+	public void showTableScreen() {
+		
+	}
+	
+	public void displayStatus(String message) {
+		
+	}
+	
+	public void displayBalance(double amount) {
+		
+	}
+	
+	public void displayCurrentBet(double amount) {
+		
+	}
+	
+	public void displayPlayerHand(String cards, int total) {
+		
+	}
+	
+	public void displayDealerHand(String cards, int total) {
+		
+	}
+	
+	public void displayRoundResult(String result) {
+		
+	}
+	
+}
