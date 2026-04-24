7a3a23bb7ee1e9c01cecdebaea092087a9a05280
Nicole (Nikki) Ong <134902567+nikkiong@users.noreply.github.com>
2026-04-23 23:06:44 -0700

    created initial card_system classes

---
+++ src/com/blackjack/game/card_system/Card.java
@@ -0,0 +1,34 @@
+package com.blackjack.game.card_system;
+
+public class Card {
+	
+	private String rank;
+	private String suit;
+	private int value;
+	private boolean faceUp;
+	
+	public void Card()
+	{
+		
+	}
+	
+	public void Card (String rank, String suit, int value, boolean faceUp)
+	{
+		
+	}
+	
+	public int getValue()
+	{
+		
+	}
+	
+	public boolean isAce()
+	{
+		
+	}
+	
+	public void flip()
+	{
+		
+	}
+}
---
+++ src/com/blackjack/game/card_system/Hand.java
@@ -0,0 +1,44 @@
+package com.blackjack.game.card_system;
+
+import java.util.*;
+
+public class Hand {
+	
+	private List<Card> cards;
+	
+	public void Hand()
+	{
+		
+	}
+	
+	public void addCard(Card card)
+	{
+		
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
+	public List<Card> gotCards()
+	{
+		
+	}
+	
+	public int gotTotal()
+	{
+		
+	}
+	
+	public boolean isBust()
+	{
+		
+	}
+	
+	public boolean isBlackjack()
+	{
+		
+	}
+}
+
