a8e4cbf37e3988f5b51ec7c88f59f89dcc1bb5eb
Nicole (Nikki) Ong <134902567+nikkiong@users.noreply.github.com>
2026-04-28 01:02:55 -0700

    Create Constructor + add UI variables & JPanel creation methods

--- src/com/blackjack/client/gui/GUIManager.java
+++ src/com/blackjack/client/gui/GUIManager.java
@@ -1,7 +1,8 @@
 package com.blackjack.client.gui;
 
 // components for GUI
-import java.awt.BorderLayout; 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.FlowLayout;
 import java.awt.Image;
 
@@ -13,12 +14,66 @@ import javax.swing.text.JTextComponent;
 
 public class GUIManager {
 
-	private Client client;
-	private String currentScreen;
+	private Client client;										 // reference to client for GUI to send messages
+	private String currentScreen;								 // variable for tracking which screen is currently active for user
 	
-	public GUIManager() {
+	JFrame frame;												 // main window
+	JPanel mainPanel, 											 // holds all differnt screens (login, lobby, table)
+		   loginPanel, lobbyPanel, tablePanel;  				 // individual screens
+	
+	JLabel statusLbl, balanceLbl, currBetLbl, pHandLbl, dHandLbl;// table screen labels
+	
+	public GUIManager(Client client) {
 		
+		this.client = client; // reference to specific client
+		
+		// Create a frame; a window displaying to the user
+		frame = new JFrame("BlackJack Game");	
+		frame.setSize(1100,700);		       				  // set window size length x width
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // make it so the window closes at exit
+		
+		// Create main panel that will hold the 3 different screens (login, lobby, table)
+		mainPanel = new JPanel();					
+		mainPanel.setLayout(new CardLayout()); 				  // Use CardLayout layout for switching between multiple views
+		
+		// add screens to the main panel (used for switching between screens)
+		loginPanel = createLoginPanel();
+		lobbyPanel = createLobbyPanel();
+		tablePanel = createTablePanel();
+		
+		// add main panel to frame/window
+		frame.add(mainPanel);
+		
+		//Set visibility to true to display on screen
+		frame.setVisible(true);
+		
+		showLoginScreen(); 								     // starting screen to play game (login screen)
 	}
+	
+	/****************************************************
+	 * 				CREATE DIFFERENT SCREENS
+	 ****************************************************/
+	private JPanel createLoginPanel() 
+	{
+		
+		// return the panel
+	}
+	
+	private JPanel createLobbyPanel() 
+	{
+		
+		// return the panel
+	}
+	
+	private JPanel createTablePanel() 
+	{
+		
+		// return the panel
+	}
+	
+	/****************************************************
+	 * 				SWITCH BETWEEN SCREENS
+	 ****************************************************/
 	
 	public void showLoginScreen() {
 		
@@ -31,6 +86,10 @@ public class GUIManager {
 	public void showTableScreen() {
 		
 	}
+	
+	/****************************************************
+	 * 		UPDATE IN GAME STATUS DISPLAY METHODS 
+	 ****************************************************/
 	
 	public void displayStatus(String message) {
 		
