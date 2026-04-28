d940af5bf7be015c82981212133ff21ca67b997a
Nicole (Nikki) Ong <134902567+nikkiong@users.noreply.github.com>
2026-04-27 23:14:52 -0700

    create default+main constructors + add comments

--- src/com/blackjack/game/card_system/Card.java
+++ src/com/blackjack/game/card_system/Card.java
@@ -1,20 +1,31 @@
 package com.blackjack.game.card_system;
 
+// Represents a single playing card
 public class Card {
 	
-	private String rank;
-	private String suit;
-	private int value;
-	private boolean faceUp;
+	private String rank;    // rank of a card (ex: "Jack", "Ace", "2")
+	private String suit;	// suits of a card (ex: 
+	private int value;		// In game Blackjack value of the card (ex: 2-10 for number cards, king/queen/jack = 10, Ace = 11 initially)
+	private boolean faceUp; // represents if card is faced up/true/visible or faced down/hidden/false
 	
-	public void Card()
+	// Default constructor that creates an "empty" card
+	public Card()
 	{
-		
+		// initializes with default placeholder values
+		rank = "";
+		suit = "";
+		value = 0;
+		faceUp = false; // initializes as hidden by default
 	}
 	
-	public void Card (String rank, String suit, int value, boolean faceUp)
+	// Main Constructor to create the cards
+	public Card (String rank, String suit, int value, boolean faceUp)
 	{
-		
+		// Assigns card values (ex: "Ace", "Hearts", 11, true (visible/faced up)
+		this.rank = rank;
+		this.suit = suit;
+		this.value = value;
+		this.faceUp = faceUp;
 	}
 	
 	public int getValue()
