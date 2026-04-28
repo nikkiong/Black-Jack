c252945bb73368573ac041a5fb52790f47e3c17e
Daniel Alvarado <danielalvarado1738@gmail.com>
2026-04-28 11:50:14 -0700

    added Card, Deck, Hand classes and their JUnit tests

--- src/com/blackjack/game/card_system/Card.java
+++ src/com/blackjack/game/card_system/Card.java
@@ -1,34 +1,56 @@
 package com.blackjack.game.card_system;
 
 public class Card {
-	
-	private String rank;
-	private String suit;
-	private int value;
-	private boolean faceUp;
-	
-	public void Card()
-	{
-		
-	}
-	
-	public void Card (String rank, String suit, int value, boolean faceUp)
-	{
-		
-	}
-	
-	public int getValue()
-	{
-		
-	}
-	
-	public boolean isAce()
-	{
-		
-	}
-	
-	public void flip()
-	{
-		
-	}
+
+    private String rank; // cards rank ex: Ace, King
+    
+    private String suit;// cards suit ex: Hearts
+    
+    private int value;// cards number value
+    
+    private boolean faceUp;// is card visible
+
+    // default constructor
+    public Card() {
+        this.rank = "";
+        this.suit = "";
+        this.value = 0;
+        this.faceUp = true;
+    }
+
+    public Card(String rank, String suit, int value, boolean faceUp) {// constructor with all values
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+        this.faceUp = faceUp;
+    }
+
+    public String getRank() {// returns the rank
+        return rank;
+    }
+
+    public String getSuit() {// returns the suit
+        return suit;
+    }
+
+    public int getValue() {// returns the value
+        return value;
+    }
+    
+    public void setValue(int value) {// sets the value
+        this.value = value;
+    }
+
+    public boolean isAce() {// checks if ace
+        return rank.equals("Ace");
+    }
+
+    
+    public void flip() {// flips card visibility
+        faceUp = !faceUp;
+    }
+
+    public boolean isFaceUp() {// checks if face up
+        return faceUp;
+    }
 }
