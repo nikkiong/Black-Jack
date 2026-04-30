0370610333db52c7f36c066e2243be94380a7bc9
Chelmati <schelmatikary2@horizon.csueastbay.edu>
2026-04-30 16:27:50 -0700

    server work

--- src/com/blackjack/server/Server.java
+++ src/com/blackjack/server/Server.java
@@ -1,5 +1,24 @@
 package com.blackjack.server;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Server {
+	ServerSocket ss;
 	
+	Server() throws IOException {
+	}
+	
+	public static void main(String args[]) throws IOException, ClassNotFoundException {
+		System.out.println("Test");
+		
+		
+		
+	}
+	
+	public void startServer() throws IOException, ClassNotFoundException {
+		//Server socket
+		this.ss = new ServerSocket(7777);
+		System.out.println("Server socket connected");
+		
+	}
 }
